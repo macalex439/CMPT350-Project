e701b0dc0df18e88986db4900db18a736beef601
macalex439 <47799266+macalex439@users.noreply.github.com>
2019-03-24 17:14:29 -0600

    Added Files for Json and register and home screen

---
+++ WorkoutSite/register.js
@@ -0,0 +1,37 @@
+function register(){
+	var xhttp = new XMLHttpRequest();
+	var user = document.getElementById("createuser").value;
+	var pw = document.getElementById("createpassword").value;
+	var cpw = document.getElementById("confirmpassword").value;
+	
+	if (pw == '' || user == '' || cpw == '') return;
+	
+	if (pw != cpw) {
+		document.getElementById("response").color = "red";
+		document.getElementById("response").innerHTML = "Passwords Do Not Match";
+		document.getElementById("createpassword").value = '';
+		document.getElementById("confirmpassword").value = '';
+		return;
+	}
+	
+	document.getElementById("createuser").value = '';
+	document.getElementById("createpassword").value = '';
+	document.getElementById("confirmpassword").value = '';
+	
+  	xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+    		if (this.responseText == "true"){
+    		    document.getElementById("response").color = "green";
+				document.getElementById("response").innerHTML = "User Successfully Created";
+    		}
+    		else if (this.responseText == "false"){
+    			document.getElementById("response").color = "red";
+				document.getElementById("response").innerHTML = "That Username Is Already In Use";
+    		}
+    	}
+  	};
+  	
+  	xhttp.open("POST", "Register" , true);
+  	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  	xhttp.send("User=" + user + "&Password=" + pw);
+}
